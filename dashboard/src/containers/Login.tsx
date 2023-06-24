940acd397d5385b11de44970ae97e8acdc24f9d3
rob <rob@gmail.com>
2023-06-24 11:20:59 +0300

    new dashboard style currently in login

--- dashboard/src/containers/Login.tsx
+++ dashboard/src/containers/Login.tsx
@@ -4,6 +4,7 @@ import {
   import "./containers.css";
   import axios from 'axios';
   import React, { useState } from 'react';
+import { Header } from "../components/header";
 
 export function SignIn() {
     const [data, setData] = useState('');
@@ -27,40 +28,57 @@ export function SignIn() {
   
     return (
       <div className="white-page">
-        <h2>Sign-In</h2>
-        <div className="row-box">
-          <div className="box">
-            <Link to="/login">Login</Link>
-          </div>        
-          <div className="box">
-            <Link to="/register">Register</Link>
-          </div>
+        <Header />
+        <div className="container">
+            <div className="menuBox">
+                <div className="menuItem">
+                    Home
+                </div>
+                <div className="menuSep" />
+                <div className="menuItemSelected">
+                    SignIn
+                </div>
+                <div className="menuSep" />
+                <div className="menuItem">
+                    Settings
+                </div>
+                <div className="menuSepLong" />
+            </div>
+            <h2>Sign-In</h2>
+            <div className="row-box">
+            <div className="box">
+                <Link to="/login">Login</Link>
+            </div>        
+            <div className="box">
+                <Link to="/register">Register</Link>
+            </div>
+            </div>
+            <form onSubmit={handleSubmit}>
+            <div className="column-box">
+                <div className="row-box">
+                <label>
+                    Email: <input name="email" type="text" />
+                </label>
+                </div>
+                <div className="row-box">
+                <label>
+                    Password: <input name="password" type="password" />
+                </label>
+                </div>
+                <div className="empty" />
+                <div className="row-box">
+                <button type="submit">Login</button>
+                </div>
+                <div className="empty" />
+                <div className="row-box">
+                <label>
+                    Token (you will get it after login): <br />
+                    <input name="token" type="text" readOnly value={data} maxLength={8048} size={200} />
+                </label>
+                </div>
+            </div>
+            </form>
         </div>
-        <form onSubmit={handleSubmit}>
-          <div className="column-box">
-            <div className="row-box">
-              <label>
-                Email: <input name="email" type="text" />
-              </label>
-            </div>
-            <div className="row-box">
-              <label>
-                Password: <input name="password" type="password" />
-              </label>
-            </div>
-            <div className="empty" />
-            <div className="row-box">
-              <button type="submit">Login</button>
-            </div>
-            <div className="empty" />
-            <div className="row-box">
-              <label>
-                Token (you will get it after login): <br />
-                <input name="token" type="text" readOnly value={data} maxLength={8048} size={200} />
-              </label>
-            </div>
-          </div>
-        </form>
       </div>
     );
   }
